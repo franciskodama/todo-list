6ddf2d78efa73135789965735eff4fee423f05d2
Kodama <fk@fkodama.com>
2020-12-26 18:10:55 -0300

    Add filter and animation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # todo-list
 First project to learn JS
+24/12/2020
+By Kodama
+
--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 
 
 //EVENT LISTENERS -------------------------
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
 
 
@@ -22,7 +24,7 @@ function addTodo(event) {
     todoDiv.classList.add('todo');
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     
     //Append the text to <li>
@@ -36,9 +38,59 @@ function addTodo(event) {
     //Check TRASH Button
     const trashButton = document.createElement('button');
     trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('complete-btn');
+    trashButton.classList.add('trash-btn');
     todoDiv.appendChild(trashButton);
 
     //Append todoDiv to todoList <ul>
     todoList.appendChild(todoDiv); 
+
+    //Clear Todo Input Value
+    todoInput.value = "";
 }   
+
+function deleteCheck(e) {
+    // Use it to test which button you are clicking on -> console.log(e.target);
+
+    const item = e.target
+    // DELETE Button
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+    }
+
+    //CHECK MARK Button
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });  
+}
+
